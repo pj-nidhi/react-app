395ca372a28244f93a3553417d34d5b714ccb326
Pankaj Nidhi <pj_nidhi@Pankajs-MacBook-Air.local>
2025-07-13 10:17:16 +0530

    final working project

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import path from "path";
+// import authMiddleware from "./authMiddleware.js";
 
 import { connectDB } from "./config/db.js";
 
@@ -17,14 +18,19 @@ app.use(express.json()); // allows us to accept JSON data in the req.body
 
 app.use("/api/products", productRoutes);
 
+
+// Import the authorization middleware
+// This middleware will handle user authentication and authorization
+// app.use(authMiddleware);
 if (process.env.NODE_ENV === "production") {
 	app.use(express.static(path.join(__dirname, "/frontend/dist")));
 	app.get("*", (req, res) => {
-		res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+		res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
 	});
 }
 
 app.listen(PORT, () => {
 	connectDB();
-	console.log("Server started at http://localhost:" + PORT);
+	const now = new Date().toISOString();
+	console.info(`[INFO] [${now}] Server started at http://localhost:${PORT}`);
 });
